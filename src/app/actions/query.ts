7f74cdf48d63890586ce555ec4f6b03fb43788af
KedarPanchal <kedar.t.panchal@outlook.com>
2025-07-23 21:54:32 -0500

    Add RAG generation (console output)

---
+++ src/app/actions/query.ts
@@ -0,0 +1,81 @@
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+
+import { ChatGroq } from "@langchain/groq";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { pipeline } from "@xenova/transformers";
+import { RunnablePassthrough, RunnableSequence } from "@langchain/core/runnables";
+import type { Document } from "@langchain/core/documents";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+// Code to set up export
+async function getEmbeddings(documents: Array<string>) {
+    const embedder = await pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2");
+    const embeddings = [];
+
+    for (const document in documents) {
+    const output = await embedder(document, {
+        pooling: "mean",
+        normalize: true,
+        });
+    embeddings.push(Array.from(output.data));
+    }
+
+    return embeddings;
+}
+
+const loader = new DirectoryLoader("src/context", {
+    ".txt": (path) => new TextLoader(path)
+});
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1024, chunkOverlap: 128
+});
+const documents = await loader.load();
+const splits = await splitter.splitDocuments(documents);
+
+const embeddings = {
+    embedDocuments: getEmbeddings,
+    embedQuery: async (query: string) => (await getEmbeddings([query]))[0],
+}
+const vectorStore = await MemoryVectorStore.fromDocuments(splits, embeddings);
+const retriever = vectorStore.asRetriever({
+    k: 10,
+    searchType: "mmr",
+    searchKwargs: {
+        fetchK: 50,
+        lambda: 0.9
+    }
+});
+
+const model = new ChatGroq({
+    model: "llama3-70b-8192",
+    temperature: 0.4,
+    maxTokens: 768,
+});
+
+// Export function
+export async function query(prompt: FormData) {
+    "use server"
+    const RAG_TEMPLATE = `Your task is to answer the given questions about Kedar Panchal using only the provided context. Never include details not present in the provided context.
+    If you don't know the answer, say "I don't know." Answer in complete sentences, and use as many examples from the context as possible. Use correct grammar.
+
+    Question: {question}
+    Context: {context}
+    Answer:`;
+    const rag_prompt = ChatPromptTemplate.fromTemplate(RAG_TEMPLATE);
+
+    const chain = RunnableSequence.from([
+      {
+        context: retriever.pipe((documents: Document[]) => (documents.map(document => document.pageContent)).join("\n\n")),
+        question: new RunnablePassthrough()
+      },
+      rag_prompt,
+      model,
+      new StringOutputParser()
+    ]);
+
+    const answer = await chain.invoke(prompt.get("question") as string);
+    console.log(answer);
+  }
