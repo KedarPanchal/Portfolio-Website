20ff5351151cc1af428897887d0a2b1ed4c2df86
KedarPanchal <kedar.t.panchal@outlook.com>
2025-07-31 13:30:00 -0500

    Fix tabbing

--- src/app/components/chatbot.tsx
+++ src/app/components/chatbot.tsx
@@ -2,36 +2,36 @@ import styles from "./chatbot.module.css";
 import { useState } from "react";
 
 export function ChatbotBlock() {
-  const defaultString = "I'm Kedar's AI assistant, ready to talk about his resume and work experience! Please ask me a question!";
-  const [chatbotQuestion, setChatbotQuestion] = useState((<p>{defaultString}</p>));
-  const [chatbotMessage, setChatbotMessage] = useState("");
+    const defaultString = "I'm Kedar's AI assistant, ready to talk about his resume and work experience! Please ask me a question!";
+    const [chatbotQuestion, setChatbotQuestion] = useState((<p>{defaultString}</p>));
+    const [chatbotMessage, setChatbotMessage] = useState("");
 
-  async function getMessage(formData: FormData) { 
-    if (formData.get("question")) {
-      setChatbotQuestion((<p><b>You Asked: </b>{formData.get("question")! as string}</p>));
-      const response = await fetch("/api/query", {
-        method: "POST",
-        body: formData.get("question"),
-      });
+    async function getMessage(formData: FormData) { 
+        if (formData.get("question")) {
+            setChatbotQuestion((<p><b>You Asked: </b>{formData.get("question")! as string}</p>));
+            const response = await fetch("/api/query", {
+                method: "POST",
+                body: formData.get("question"),
+            });
 
-      const responseJSON = await response.json();
-      setChatbotMessage(responseJSON.message);
-    } else {
-      setChatbotMessage("");
-      setChatbotQuestion((<p>{defaultString}</p>));
+            const responseJSON = await response.json();
+            setChatbotMessage(responseJSON.message);
+        } else {
+            setChatbotMessage("");
+            setChatbotQuestion((<p>{defaultString}</p>));
+        }
     }
-  }
 
-  return (
-    <div className={styles.chatbotBlock}>
-      <form action={getMessage} className={styles.chatbotInputForm}>
-        <input name="question" type="search" autoComplete="off" className={styles.chatbotInputText}></input>
-        <button type="submit" className={styles.chatbotInputButton}>{"↵"}</button>
-      </form>  
-      <div className={styles.chatbotOutput}>
-        {chatbotQuestion}
-        <p className={styles.chatbotOutputResponse}>{chatbotMessage}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.chatbotBlock}>
+            <form action={getMessage} className={styles.chatbotInputForm}>
+                <input name="question" type="search" autoComplete="off" className={styles.chatbotInputText}></input>
+                <button type="submit" className={styles.chatbotInputButton}>{"↵"}</button>
+            </form>  
+            <div className={styles.chatbotOutput}>
+                {chatbotQuestion}
+                <p className={styles.chatbotOutputResponse}>{chatbotMessage}</p>
+            </div>
+        </div>
+    );
 }
