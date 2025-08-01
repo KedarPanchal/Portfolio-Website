1795393f4cfa8e4e5adff29dba01eeec023ee1e2
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-01 15:05:35 -0500

    Implement menu scrolling for smaller devices

--- src/app/components/toolbar.tsx
+++ src/app/components/toolbar.tsx
@@ -2,12 +2,42 @@
 
 import Link from "next/link";
 import styles from "./toolbar.module.css";
+import { useRef, useEffect } from "react";
+import { RefObject } from "react";
 
-export function Toolbar() {
+interface ToolbarProps {
+    root: RefObject<HTMLDivElement|null>
+}
+export function Toolbar({root}: ToolbarProps) {
+    type ToolbarKey = "about" | "chatbot";
+    const toolbarRefs: Record<ToolbarKey, RefObject<HTMLAnchorElement|null>> = {
+        about: useRef<HTMLAnchorElement>(null),
+        chatbot: useRef<HTMLAnchorElement>(null),
+    }
+
+    useEffect(() => {
+        const menuObserver = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    toolbarRefs[entry.target.id as ToolbarKey].current!.scrollIntoView({
+                        behavior: "smooth",
+                        inline: "center",
+                    });
+                }
+            })
+        }, {
+            threshold: 0.95,
+            root: root.current,
+        });
+
+        root.current!.querySelectorAll("section").forEach((section) => menuObserver.observe(section));
+        return () => menuObserver.disconnect();
+    }, []);
+    
     return (
         <nav className={styles.toolbar}>
-            <h1><Link href="#about" style={{ textDecoration: "none" }}>Kedar Panchal</Link></h1>
-            <h2><Link href="#chatbot" style={{ textDecoration: "none" }}>AI Assistant</Link></h2>
+            <h1><Link href="#about" ref={toolbarRefs["about"]} style={{ textDecoration: "none" }}>Kedar Panchal</Link></h1>
+            <h2><Link href="#chatbot" ref={toolbarRefs["chatbot"]} style={{ textDecoration: "none" }}>AI Assistant</Link></h2>
             <h2><Link href="/" style={{ textDecoration: "none" }}>Experience</Link></h2>
             <h2><Link href="/" style={{ textDecoration: "none" }}>Projects</Link></h2>
             <h2><Link href="/" style={{ textDecoration: "none" }}>Certifications</Link></h2>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 "use client"
 
 import styles from "./page.module.css";
+
 import { ChatbotBlock } from "./components/chatbot";
 import { AboutMeBlock } from "./components/aboutme";
 import { Toolbar } from "./components/toolbar";
+
+import { useRef } from "react";
+
 import { env } from "@xenova/transformers";
 
 env.allowLocalModels = true;
 
 export default function Home() {
+  const mainPageRef = useRef<HTMLDivElement>(null);
   return (
-    <div className={styles.page}>
-      <Toolbar />
+    <div className={styles.page} ref={mainPageRef}>
+      <Toolbar root={mainPageRef}/>
       <section id="about">
         <AboutMeBlock />
       </section>
