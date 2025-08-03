2a6e9c4652364185755ce5eb48136b7c53172de9
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-03 00:00:32 -0500

    Use HTML entities instead of JSX in About Me

--- src/app/components/aboutme.tsx
+++ src/app/components/aboutme.tsx
@@ -8,7 +8,7 @@ export function AboutMeBlock() {
         <div className={styles.chatbotBlock}>
             <Image src={profileImage} alt="A picture of Kedar Panchal" className={styles.profileImage}/>
             <p className={styles.aboutMeText}>
-                Hi, {"I'm"} <span className={styles.name}>Kedar Panchal</span>, a Computer Science and Engineering Honors student at Texas A&M with a passion for artificial intelligence and its potential to create innovative solutions.
+                Hi, I&#39;m <span className={styles.name}>Kedar Panchal</span>, a Computer Science and Engineering Honors student at Texas A&M with a passion for artificial intelligence and its potential to create innovative solutions.
                 Feel free to scroll down to interact with an <b>AI assistant</b> to learn about my projects, skills, work experience, and more, or explore my website further to view this information in-depth.
             </p>
         </div>
