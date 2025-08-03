a61a90c3eb557b6f74b60390bacc5d40fa69350d
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-02 23:14:40 -0500

    Start basic project widget code

---
+++ src/app/components/projectwidget.tsx
@@ -0,0 +1,26 @@
+import styles from "./projectwidget.module.css";
+
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+import { Url } from "url";
+
+interface ProjectWidgetProps {
+    href: string,
+    imageSrc: StaticImageData,
+    altText?: string,
+    widgetTitle: string,
+    widgetDescription: string,
+}
+export function ProjectWidget({href, imageSrc, altText="", widgetTitle, widgetDescription}: ProjectWidgetProps) {
+    return (
+        <Link href={href} target="_blank" className={styles.widgetBlock}>
+            <div className={styles.widgetTop}>
+                <Image src={imageSrc} alt={altText} className={styles.widgetImage}/>
+                <h1 className={styles.widgetTitle}>{widgetTitle}</h1>
+            </div>
+            <div className={styles.widgetBottom}>
+                <p className={styles.widgetDescription}>{widgetDescription}</p>
+            </div>
+        </Link>
+    );
+}
