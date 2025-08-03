a94a2b46869006f1db01c98e8ed147d33529f5d2
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-02 20:28:12 -0500

    Finish about me section

--- src/app/components/aboutme.tsx
+++ src/app/components/aboutme.tsx
@@ -5,8 +5,12 @@ import profileImage from "../../public/images/me.png";
 
 export function AboutMeBlock() {
     return (
-        <div>
+        <div className={styles.chatbotBlock}>
             <Image src={profileImage} alt="A picture of Kedar Panchal" className={styles.profileImage}/>
+            <p className={styles.aboutMeText}>
+                Hi, {"I'm"} <span className={styles.name}>Kedar Panchal</span>, a Computer Science and Engineering Honors student at Texas A&M with a passion for artificial intelligence and its potential to create innovative solutions.
+                Feel free to scroll down to interact with an <b>AI assistant</b> to learn about my projects, skills, work experience, and more, or explore my website further to view this information in-depth.
+            </p>
         </div>
     )
 }
