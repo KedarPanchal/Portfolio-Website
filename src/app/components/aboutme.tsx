4c97eb08fd16b2d3d388653d02e7732a0bd45cdf
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-03 20:37:22 -0500

    Update about me to have grid layout

--- src/app/components/aboutme.tsx
+++ src/app/components/aboutme.tsx
@@ -9,7 +9,7 @@ export function AboutMeBlock() {
             <Image src={profileImage} alt="A picture of Kedar Panchal" className={styles.profileImage}/>
             <p className={styles.aboutMeText}>
                 Hi, I&#39;m <span className={styles.name}>Kedar Panchal</span>, a Computer Science and Engineering Honors student at Texas A&M with a passion for artificial intelligence and its potential to create innovative solutions.
-                Feel free to scroll down to interact with an <b>AI assistant</b> to learn about my projects, skills, work experience, and more, or explore my website further to view this information in-depth.
+                Feel free to scroll down to interact with an <b>AI assistant</b> to learn about my projects, skills, work experience, and more, or <b>explore my website further</b> to view this information in-depth.
             </p>
         </div>
     )
