21cc9d32f98a999d8449af81cf0dbb193f51bab2
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-05 16:07:27 -0500

    Add type robustness to work experience

--- src/app/components/workexperience.tsx
+++ src/app/components/workexperience.tsx
@@ -4,18 +4,32 @@ import styles from "./workexperience.module.css";
 
 import { useState } from "react";
 
+type Month = "Jan" | "Feb" | "Mar" | "Apr" | "May" | "Jun" | "Jul" | "Aug" | "Sep" | "Oct" | "Nov" | "Dec";
+type JobDate = {
+    startMonth: Month,
+    startYear: number,
+    endMonth?: Month,
+    endYear: number | "Present",
+}
 interface WorkExperienceProps {
     jobName: string,
     workplaceName: string,
+    date: JobDate,
+    description: string[],
 }
-function WorkExperience({jobName, workplaceName}: WorkExperienceProps) {
+function WorkExperience({jobName, workplaceName, date, description}: WorkExperienceProps) {
     return (
         <div className={styles.workExperience}>
             <div className={styles.jobTitle}>
                 <h1 className={styles.jobTitleText}><span style={{ fontWeight: "normal" }}>{jobName}</span> - <em>{workplaceName}</em></h1>
             </div>
             <div className={styles.jobDescription}>
-                <p className={styles.jobDescriptionText}></p>
+                <p className={styles.jobDate}>{date.startMonth} {date.startYear} - {date.endMonth ?? ""} {date.endYear}</p>
+                <ul className={styles.jobDescriptionText}>
+                    {description.map((line) => {
+                        return <li>{line}</li>
+                    })}
+                </ul>
             </div>
         </div>
     );
@@ -23,15 +37,100 @@ function WorkExperience({jobName, workplaceName}: WorkExperienceProps) {
 
 export function WorkExperienceBlock() {
     const [experienceIndex, setExperienceIndex] = useState(0);
-    const workExperienceArr = [
-        (<WorkExperience jobName="Automation Developer" workplaceName="MathNMore LLC" key={0} />),
-        (<WorkExperience jobName="Assistant Project Manager" workplaceName="Recharge Realty LLC" key={1} />),
-        (<WorkExperience jobName="Vice President of Operations" workplaceName="McNeil DECA" key={2} />),
-        (<WorkExperience jobName="Academic Tutor" workplaceName="Eye Level Learning" key={3} />),
-        (<WorkExperience jobName="Summer Robotics Research Intern" workplaceName="The University of Texas at Austin RobIn Lab" key={4} />),
-        (<WorkExperience jobName="Acadmic Tutor" workplaceName="Global Impact Initiative" key={5} />),
-        (<WorkExperience jobName="Founder" workplaceName="Arbitrarium Games" key={6} />),
+
+    type Job = {
+        name: string,
+        workplace: string,
+        date: JobDate,
+        description: string[],
+    }
+    const jobs: Job[] = [
+        {
+            name: "Automation Developer",
+            workplace: "MathNMore LLC",
+            date: {
+                startMonth: "Jul",
+                startYear: 2022,
+                endYear: "Present",
+            },
+            description: []
+        },
+        {
+            name: "Assistant Project Manager",
+            workplace: "Recharge Realty LLC",
+            date: {
+                startMonth: "Jul",
+                startYear: 2022,
+                endYear: "Present",
+            },
+            description: []
+        },
+        {
+            name: "Vice President of Operations",
+            workplace: "McNeil DECA",
+            date: {
+                startMonth: "Apr",
+                startYear: 2024,
+                endMonth: "May",
+                endYear: 2025,
+            },
+            description: []
+        },
+        {
+            name: "Academic Tutor",
+            workplace: "Eye Level Learning",
+            date: {
+                startMonth: "Jun",
+                startYear: 2024,
+                endMonth: "Dec",
+                endYear: 2024,
+            },
+            description: []
+        },
+        {
+            name: "Summer Robotics Research Intern",
+            workplace: "The University of Texas at Austin RobIn Lab",
+            date: {
+                startMonth: "Jun",
+                startYear: 2024,
+                endMonth: "Jul",
+                endYear: 2024,
+            },
+            description: []
+        },
+        {
+            name: "Academic Tutor",
+            workplace: "Global Impact Initiative",
+            date: {
+                startMonth: "Jun",
+                startYear: 2023,
+                endMonth: "Aug",
+                endYear: 2023
+            },
+            description: []
+        },
+        {
+            name: "Founder",
+            workplace: "Arbitrarium Games",
+            date: {
+                startMonth: "Aug",
+                startYear: 2020,
+                endMonth: "Aug",
+                endYear: 2021,
+            },
+            description: []
+        }
     ];
+
+    const workExperienceArr = jobs.map((job, i) => {
+        return <WorkExperience 
+            jobName={job.name} 
+            workplaceName={job.workplace} 
+            date={job.date} 
+            description={job.description} 
+            key={i} 
+        />
+    });
 
     const incrementExperienceIndex = () => {
         if (experienceIndex < workExperienceArr.length - 1) {
