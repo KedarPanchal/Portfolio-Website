0cc3615c6128da4dbfc028fcfc5b5638e87673a8
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-04 23:54:34 -0500

    Begin framework for work experience design

--- src/app/components/workexperience.tsx
+++ src/app/components/workexperience.tsx
@@ -4,61 +4,33 @@ import styles from "./workexperience.module.css";
 
 import { CSSProperties, useRef, useState } from "react";
 
-interface WorkExperienceWidgetProps {
-    jobTitle: string,
-    companyName: string,
-    selected?: boolean,
-    style?: CSSProperties
+interface WorkExperienceProps {
+    jobName: string,
+    workplaceName: string,
 }
-function WorkExperienceWidget({jobTitle, companyName, selected=false, style}: WorkExperienceWidgetProps) {
-    const colorScheme = selected ? styles.workExperienceWidgetSelected : styles.workExperienceWidgetUnselected;
+function WorkExperience({jobName, workplaceName}: WorkExperienceProps) {
     return (
-        <button className={`${styles.workExperienceWidget} ${colorScheme}`} style={style} onClick={() => console.log("Hello world!")}>
-            <h1 className={styles.jobTitle}>{jobTitle}</h1>
-            <p className={styles.companyName}>{companyName}</p>
-        </button>
-    );
-}
-
-function WorkExperienceDescription() {
-    return (
-        <div className={styles.WorkExperienceDescription}>
-
+        <div className={styles.workExperience}>
+            <div className={styles.jobTitle}>
+                <h1 className={styles.jobTitleText}><span style={{ fontWeight: "normal" }}>{jobName}</span> - <em>{workplaceName}</em></h1>
+            </div>
+            <div className={styles.jobDescription}>
+                <p className={styles.jobDescriptionText}></p>
+            </div>
         </div>
     );
 }
 
 export function WorkExperienceBlock() {
-    type WorkExperience = {
-        title: string;
-        company: string;
-    }
-
-    const workExperiences: WorkExperience[] = [
-        {title: "Automation Developer", company: "MathNMore LLC"},
-        {title: "Assistant Project Manager", company: "Recharge Realty LLC"},
-        {title: "Vice President of Operations", company: "DECA Inc."},
-        {title: "Summer Robotics Research Intern", company: "The University of Texas at Austin RobIn Lab"},
-        {title: "Academic Tutor", company: "Eye Level Learning"},
-        {title: "Academic Tutor", company: "Global Impact Initiative"},
-        {title: "Founder", company: "Arbitrarium Games"},
-    ];
     return (
         <div className={styles.workExperienceBlock}>
-            {workExperiences.map((experience, i, workExperiences) => {
-                const style = {
-                    borderTopLeftRadius: "0px",
-                    borderTopRightRadius: "0px",
-                }
-                if (i == 0) {
-                    style.borderTopLeftRadius = "10px";
-                }
-                if (i == workExperiences.length - 1) {
-                    style.borderTopRightRadius = "10px";
-                }
-
-                return <WorkExperienceWidget jobTitle={experience.title} companyName={experience.company} style={style} key={i}/>
-            })}
+            <button className={styles.navButton}>
+                <p>{"<"}</p>
+            </button>
+                <WorkExperience jobName="Automation Developer" workplaceName="MathNMore LLC"/>
+            <button className={styles.navButton}>
+                <p>{">"}</p>
+            </button>
         </div>
     );
 }
