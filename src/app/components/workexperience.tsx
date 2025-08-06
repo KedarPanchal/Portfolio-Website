8562f73cec06c11b99e86cc00e03f90ae24051ab
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-05 21:14:20 -0500

    Finalize work experience descriptions and navigation buttons

--- src/app/components/workexperience.tsx
+++ src/app/components/workexperience.tsx
@@ -3,6 +3,8 @@
 import styles from "./workexperience.module.css";
 
 import { useState } from "react";
+import Image from "next/image";
+import arrowHead from "../../public/images/arrowhead.png";
 
 type Month = "Jan" | "Feb" | "Mar" | "Apr" | "May" | "Jun" | "Jul" | "Aug" | "Sep" | "Oct" | "Nov" | "Dec";
 type JobDate = {
@@ -26,8 +28,8 @@ function WorkExperience({jobName, workplaceName, date, description}: WorkExperie
             <div className={styles.jobDescription}>
                 <p className={styles.jobDate}>{date.startMonth} {date.startYear} - {date.endMonth ?? ""} {date.endYear}</p>
                 <ul className={styles.jobDescriptionText}>
-                    {description.map((line) => {
-                        return <li>{line}</li>
+                    {description.map((line, i) => {
+                        return <li key={i}>{line}</li>
                     })}
                 </ul>
             </div>
@@ -53,7 +55,13 @@ export function WorkExperienceBlock() {
                 startYear: 2022,
                 endYear: "Present",
             },
-            description: []
+            description: [
+                "Created Apps Script programs to automate issuance of late notices for real estate loan payments",
+                "Explained and documented automation tools clearly to non-technical team members for them to utilize in their departments",
+                "Managed mortgage software and spreadsheets for tracking and logging real-estate loan financials",
+                "Automated tracking of property insurance premiums",
+                "Communicated with borrowers regarding late payments",
+            ],
         },
         {
             name: "Assistant Project Manager",
@@ -63,7 +71,13 @@ export function WorkExperienceBlock() {
                 startYear: 2022,
                 endYear: "Present",
             },
-            description: []
+            description: [
+                "Project-managed and performed real-estate renovation projects across Texas",
+                "Renovated class B and C properties for lease or sale",
+                "Carried out various installation, repair, maintenance, and make-ready tasks",
+                "Met with tenants to identify necessary renovations and determine best course of action",
+                "Coordinated with team-members to verify properties met move-out standards defined in lease agreement",
+            ],
         },
         {
             name: "Vice President of Operations",
@@ -74,7 +88,14 @@ export function WorkExperienceBlock() {
                 endMonth: "May",
                 endYear: 2025,
             },
-            description: []
+            description: [
+                "Managed diverse team of 9 officers",
+                "Drove recruitment programs to grow club membership to 400+ members",
+                "Developed Java, Python, and JavaScript programs to automate member merit tracking & registration",
+                "Directed IT team to maintain code",
+                "Wrote detailed documentation and style guides to ensure code maintainability during and after my term's completion",
+                "Coached members in developing professional writing, photo editing, financial projection, communication, and presentation skills",
+            ]
         },
         {
             name: "Academic Tutor",
@@ -85,7 +106,13 @@ export function WorkExperienceBlock() {
                 endMonth: "Dec",
                 endYear: 2024,
             },
-            description: []
+            description: [
+                "Tutored pre-K to high school-aged students in English and math",
+                "Designed summer camp science activities for elementary-aged students",
+                "Re-imaged older computers to use lightweight Linux distibutions for students to use for e-learning",
+                "Trained fellow coworkers on installing necessary educational software on re-imaged computers",
+                "Coordinated with fellow tutors to define personalized curriculum plans to maximize student success",
+            ],
         },
         {
             name: "Summer Robotics Research Intern",
@@ -96,7 +123,14 @@ export function WorkExperienceBlock() {
                 endMonth: "Jul",
                 endYear: 2024,
             },
-            description: []
+            description: [
+                "Conducted research at the University of Texas at Austin RobIn Interactive Intelligence Lab",
+                "Trained novel BaRiFlex robotic arm design using reinforcement machine learning techniques in performing grasping tasks",
+                "Assembled and soldered 3-D printed robot apparatus used for collecting training data",
+                "Programmed Bash scripts to automate recording sensor data as Robot Operating System Bags for storage and later use",
+                "Developed Python scripts to translate ROS bags to AI-friendly HDF5 archives",
+                "Designed Docker containers for cross-platform development using Linux-exclusive software",
+            ],
         },
         {
             name: "Academic Tutor",
@@ -107,7 +141,14 @@ export function WorkExperienceBlock() {
                 endMonth: "Aug",
                 endYear: 2023
             },
-            description: []
+            description: [
+                "Tutored elementary to high school-aged Afghan and Ukrainian refugees in English and math",
+                "Created lesson plans tailored to students' unique academic needs, language barriers, and cultural differences",
+                "Corresponded with parents to create ever-evolving, continuous learning environment",
+                "Held lessons and parent-teacher-student meetings using Zoom",
+                "Coordinated with fellow tutors to adjust lesson plans for group tutoring sessions",
+                "Re-imaged old computers with ChromeOS for students to use for online learning",
+            ],
         },
         {
             name: "Founder",
@@ -115,10 +156,15 @@ export function WorkExperienceBlock() {
             date: {
                 startMonth: "Aug",
                 startYear: 2020,
-                endMonth: "Aug",
-                endYear: 2021,
+                endMonth: "Dec",
+                endYear: 2022,
             },
-            description: []
+            description: [
+                "Designed and sold tabletop board games using The Game Crafter's digital retail platform",
+                "Used GIMP and Shotcut audiovisual editing tools to design game pieces and promotional material",
+                "Adopted image-generating AI tools like Stable Diffusion early to create concept art",
+                "Created an online following through Facebook and YouTube to market products and create informational content about the tabletop gaming industry",
+            ],
         }
     ];
 
@@ -151,11 +197,11 @@ export function WorkExperienceBlock() {
     return (
         <div className={styles.workExperienceBlock}>
             <button className={styles.navButton} onClick={decrementExperienceIndex}>
-                <p>{"<"}</p>
+                <Image src={arrowHead} alt="" className={styles.navButtonLeftArrow} />
             </button>
                 {workExperienceArr[experienceIndex]}
             <button className={styles.navButton} onClick={incrementExperienceIndex}>
-                <p>{">"}</p>
+                <Image src={arrowHead} alt="" className={styles.navButtonRightArrow} />
             </button>
         </div>
     );
