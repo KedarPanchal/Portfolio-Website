a7bfb1f3af11cf9a5f7f9a34b434efa349a40744
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-07 14:34:11 -0500

    Fix generation indicator

--- src/app/components/chatbot.tsx
+++ src/app/components/chatbot.tsx
@@ -1,18 +1,19 @@
 import styles from "./chatbot.module.css";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 export function ChatbotBlock() {
     const defaultString = "I'm Kedar's AI assistant, ready to talk about his resume and work experience. Let me know how I can help!";
     const [chatbotQuestion, setChatbotQuestion] = useState((<p>{defaultString}</p>));
     const [chatbotMessage, setChatbotMessage] = useState("");
 
-    async function getMessage(formData: FormData) { 
+    async function getMessage(event: FormEvent<HTMLFormElement>) { 
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+
         if (formData.get("question")) {
             setChatbotQuestion((<p><b>You Asked: </b>{formData.get("question")! as string}</p>));
-            
-            setChatbotMessage("Thinking...");
-            await new Promise((timeout) => setTimeout(timeout, 0));
 
+            setChatbotMessage("Thinking...");
             const response = await fetch("/api/query", {
                 method: "POST",
                 body: formData.get("question"),
@@ -28,7 +29,7 @@ export function ChatbotBlock() {
 
     return (
         <div className={styles.chatbotBlock}>
-            <form action={getMessage} className={styles.chatbotInputForm}>
+            <form onSubmit={getMessage} className={styles.chatbotInputForm}>
                 <input name="question" type="search" autoComplete="off" className={styles.chatbotInputText}></input>
                 <button type="submit" className={styles.chatbotInputButton}>{"↵"}</button>
             </form>  
