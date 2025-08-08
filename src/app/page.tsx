d563fbc3e4d2d684202712dadbffc8c1d928b0a2
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-08 00:13:49 -0500

    Add a small scroll arrow to indicate that sections are left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,19 @@ import { ChatbotBlock } from "./components/chatbot";
 import { ProjectsBlock } from "./components/projects";
 import { CertificationsBlock } from "./components/certifications";
 
+import Image from "next/image";
 import { useRef } from "react";
-
 import { env } from "@xenova/transformers";
 
+import scrollArrow from "../public/images/scrollarrow.png";
+
 env.allowLocalModels = true;
+
+function ScrollArrow() {
+  return (
+      <Image src={scrollArrow} alt="" className={styles.scrollArrowImage} />
+  );
+}
 
 export default function Home() {
   const mainPageRef = useRef<HTMLDivElement>(null);
@@ -24,15 +32,19 @@ export default function Home() {
       <Toolbar root={mainPageRef}/>
       <section id="about">
         <AboutMeBlock />
+        <ScrollArrow />
       </section>
       <section id="chatbot">
         <ChatbotBlock />  
+        <ScrollArrow />
       </section>
       <section id="experience">
         <WorkExperienceBlock />
+        <ScrollArrow />
       </section>
       <section id="projects">
         <ProjectsBlock />
+        <ScrollArrow />
       </section>
       <section id="certifications">
         <CertificationsBlock />
