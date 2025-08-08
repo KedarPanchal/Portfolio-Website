6681e0a0e016cf7a9ce6acab331caaa4a7f2d481
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-08 01:09:57 -0500

    Adjust particle configuration

--- src/app/components/particles.tsx
+++ src/app/components/particles.tsx
@@ -32,13 +32,13 @@ export function ParticleBG() {
                             enable: true,
                             mode: "repulse"
                         },
-                        resize: true
+                        resize: true,
+                    },
+                    modes: {
+                        repulse: {
+                            distance: 200,
+                            duration: 1,
                         },
-                        modes: {
-                            repulse: {
-                                distance: 200,
-                                duration: 1,
-                            },
                     }
                 },
                 particles: {
@@ -50,39 +50,43 @@ export function ParticleBG() {
                         distance: 150,
                         enable: true,
                         opacity: 0.25,
-                        width: 1
+                        width: 1,
                     },
                     collisions: {
-                        enable: true
+                        enable: true,
+                        mode: "bounce",
                     },
                     move: {
                         direction: "none",
                         enable: true,
                         outModes: {
-                            default: "bounce"
+                            default: "bounce",
                         },
                         random: false,
                         speed: 1,
-                        straight: false
+                        straight: false,
                     },
                     number: {
                         density: {
                             enable: true,
-                            area: 800
+                            area: 800,
                         },
-                        value: 80
+                        value: 80,
                     },
                     opacity: {
-                        value: 0.5
+                        value: 0.5,
                     },
                     shape: {
-                        type: "circle"
+                        type: "circle",
                     },
                     size: {
-                        value: { min: 1, max: 5 }
+                        value: { 
+                            min: 1, 
+                            max: 5, 
+                        },
                     }
                 },
-                detectRetina: true
+                detectRetina: true,
             }}
         />
         </div>
