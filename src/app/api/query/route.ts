8eedfd797afe747f0ff246a767e20923077286ca
KedarPanchal <kedar.t.panchal@outlook.com>
2025-08-18 16:58:36 -0500

    Add times to work NS club experience

--- src/app/api/query/route.ts
+++ src/app/api/query/route.ts
@@ -62,14 +62,14 @@ const retriever = await loader.load()
 const model = new ChatGroq({
     model: "llama3-70b-8192",
     temperature: 0.4,
-    maxTokens: 768,
+    maxTokens: 1024,
 });
 
 // Export function
 export async function POST(prompt: Request) {
     "use server"
-    const RAG_TEMPLATE = `Your task is to answer the given questions about Kedar Panchal in a friendly tone using only the provided context. Never include details not present in the provided context.
-    Answer in complete sentences, and use as many examples from the context as possible. Use correct grammar.
+    const RAG_TEMPLATE = `Your task is give detailed answers to the given questions about Kedar Panchal in a friendly and approachable tone using only the provided context. Never include details not present in the provided context.
+    Never give incorrect information from the context. Answer in complete sentences, and use as many examples from the context as possible. Use correct grammar and answer only in paragraph form.
 
     Question: {question}
     Context: {context}
